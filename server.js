c60d3e9c6e7f4077ae94130eb89e4c5723ef0cdb
Ahlem AHMED <ahmedahlem3@gmail.com>
2021-06-16 00:01:58 +0100

    server modified

--- server.js
+++ server.js
@@ -6,21 +6,36 @@ const path = require ('path')
 const app=express()
 require ('dotenv').config()
 
-
+const whitelist = ['http://localhost:3000', 'http://localhost:5000']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
 connectDB()
 
 app.use(bodyParser.json());
-app.use(cors());
+
 //API Route
 app.use("/api/user/", require("./routes/User"));
 // app.use("/api/profile/", require("./routes/Profile"));
 app.use("/api/property/", require("./routes/Property"));
 
-if(process.env.NODE_ENV === 'production'){
-    app.usr(express.static('client/build'));
-    app.get('*', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 }
 const port=process.env.PORT || 5000
 app.listen(port,(error)=>
